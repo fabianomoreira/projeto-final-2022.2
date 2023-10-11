5d6efe36e2d2fcaa3c464192721d23fc54326666
Daniel Nogueira <113999917+danielzinhonog@users.noreply.github.com>
2023-10-11 19:51:38 -0300

    Update Banco de dados.sql
    Criação da tabela plano para os tipos de planos de assinatura que estarão presentes no site, e acréscimo de alguns detalhes.

--- Banco de dados/Banco de dados.sql
+++ Banco de dados/Banco de dados.sql
@@ -3,25 +3,32 @@ CREATE DATABASE donna_encrenca;
 USE donna_encrenca;
 
 CREATE TABLE profissao (
-	id_profissao INT(255) AUTO_INCREMENT PRIMARY KEY,
-    nome_profissao VARCHAR(255)
+	id_profissao INT AUTO_INCREMENT PRIMARY KEY,
+    nome_profissao VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE localidade (
-	id_localidade INT(255) AUTO_INCREMENT PRIMARY KEY,
-    nome_localidade VARCHAR(255)
+	id_localidade INT AUTO_INCREMENT PRIMARY KEY,
+    nome_localidade VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE documentos (
 	id_documentos INT AUTO_INCREMENT PRIMARY KEY,
     img_doc BLOB,
-    data_upload DATETIME
+    data_upload DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE avaliacao (
     id_avaliacao INT AUTO_INCREMENT PRIMARY KEY,
     nota DECIMAL(3, 1) NOT NULL,
-    comentario TEXT
+    comentario TEXT,
+    data_avaliacao DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE plano (
+    id_plano INT AUTO_INCREMENT PRIMARY KEY,
+    valor DECIMAL(5, 2) NOT NULL,
+    descricao TEXT
 );
 
 CREATE TABLE usuario (
@@ -30,16 +37,18 @@ CREATE TABLE usuario (
     telefone VARCHAR(15) NOT NULL,
     cpf VARCHAR(11) NOT NULL UNIQUE,
     email VARCHAR(50) NOT NULL UNIQUE,
-    senha VARCHAR(50) NOT NULL,
+    senha VARCHAR(60) NOT NULL,
     img_perfil BLOB,
-    data_upload DATETIME,
+    data_upload DATETIME DEFAULT CURRENT_TIMESTAMP,
     descricao TEXT,
     id_profissao INT,
     id_localidade INT,
     id_documentos INT,
     id_avaliacao INT,
+    id_plano INT,
 	CONSTRAINT fk_docUsu FOREIGN KEY (id_documentos) REFERENCES documentos(id_documentos),
 	CONSTRAINT fk_profUsu FOREIGN KEY (id_profissao) REFERENCES profissao(id_profissao),
 	CONSTRAINT fk_localUsu FOREIGN KEY (id_localidade) REFERENCES localidade(id_localidade),
-	CONSTRAINT fk_avalUsu FOREIGN KEY (id_avaliacao) REFERENCES avaliacao(id_avaliacao)
+	CONSTRAINT fk_avalUsu FOREIGN KEY (id_avaliacao) REFERENCES avaliacao(id_avaliacao),
+    CONSTRAINT fk_planUsu FOREIGN KEY (id_plano) REFERENCES plano(id_plano)
 );
