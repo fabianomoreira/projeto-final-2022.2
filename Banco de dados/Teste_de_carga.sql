1b0772627944677a9d0716689248b98cae0bb83d
João Vitor Barbosa <113999841+JoaoVBarbosa7@users.noreply.github.com>
2023-10-16 10:01:10 -0300

    Update and rename Teste_de_carga to Teste_de_carga.sql
    Correção dos exemplos para nomes femininos

---
+++ Banco de dados/Teste_de_carga.sql
@@ -0,0 +1,20 @@
+SELECT * FROM usuario;
+insert into usuario(nome,telefone,cpf,email,senha,descricao) values("Roberta Silva","40028922","11111111111","roberta.silva@email.com","vascodagama","Descrição da Roberta Silva");
+insert into usuario(nome,telefone,cpf,email,senha,descricao) values("Daniela Martins","123456789","741852963","emailDaniela@provedor.com","senhaForte","Descrição da Daniela Martins");
+insert into usuario(nome,telefone,cpf,email,senha,descricao) values("Juliana Muller","987654321","147258369","jumuller77@email.com","12345678","Descrição da Juliana Muller ");
+insert into usuario(nome,telefone,cpf,email,senha,descricao) values("Luani Filho","963852741","654987321","lulu.princesa@email.com","princesa123","Descrição da Luani Filho");
+insert into usuario(nome,telefone,cpf,email,senha,descricao) values("Mariana Pessoa","321654987","321987456","marip@email.com","pipocadoce","Descrição aqui da Mariana Pessoa");
+
+SELECT * FROM localidade;
+insert into localidade(nome_localidade) values("Itaboraí");
+insert into localidade(nome_localidade) values("Magé");
+insert into localidade(nome_localidade) values("Niterói");
+insert into localidade(nome_localidade) values("Rio de janeiro");
+insert into localidade(nome_localidade) values("São Gonçalo");
+
+SELECT * FROM profissao;
+insert into profissao(nome_profissao) values("eletricista");
+insert into profissao(nome_profissao) values("marceneira");
+insert into profissao(nome_profissao) values("mecânica");
+insert into profissao(nome_profissao) values("motorista");
+insert into profissao(nome_profissao) values("piscineira");
