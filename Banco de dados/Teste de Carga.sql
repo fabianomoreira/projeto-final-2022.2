dd4d323627474a1b95bc7634f0d1dda82d34b540
João Vitor Barbosa <113999841+JoaoVBarbosa7@users.noreply.github.com>
2023-10-18 19:22:04 -0300

    Update Teste de Carga.sql

--- Banco de dados/Teste de Carga.sql
+++ Banco de dados/Teste de Carga.sql
@@ -5,20 +5,6 @@ insert into usuario(nome,telefone,cpf,email,senha,descricao) values("Juliana Mul
 insert into usuario(nome,telefone,cpf,email,senha,descricao) values("Luani Filho","963852741","654987321","lulu.princesa@email.com","princesa123","Descrição da Luani Filho");
 insert into usuario(nome,telefone,cpf,email,senha,descricao) values("Mariana Pessoa","321654987","321987456","marip@email.com","pipocadoce","Descrição aqui da Mariana Pessoa");
 
-SELECT * FROM localidade;
-insert into localidade(nome_localidade) values("Itaboraí");
-insert into localidade(nome_localidade) values("Magé");
-insert into localidade(nome_localidade) values("Niterói");
-insert into localidade(nome_localidade) values("Rio de janeiro");
-insert into localidade(nome_localidade) values("São Gonçalo");
-
-SELECT * FROM profissao;
-insert into profissao(nome_profissao) values("eletricista");
-insert into profissao(nome_profissao) values("marceneira");
-insert into profissao(nome_profissao) values("mecânica");
-insert into profissao(nome_profissao) values("motorista");
-insert into profissao(nome_profissao) values("piscineira");
-
 select * from plano;
 
 insert into plano(nome_plano, valor, descricao) values ("mensal","30.00", "com o nosso plano mensal, você terá direito de anunciar o seu serviço na nossa plataforma");
